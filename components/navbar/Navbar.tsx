9daef0405555601711e22a76d98a6f2b20f4b731
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-07-21 17:07:47 +0530

    navbar updated

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import AuthLinks from '../authLinks/AuthLinks'
+import ThemeToggle from '../themeToggle/ThemeToggle'
 
 const Navbar = () => {
     return (
-        <div className={styles.container}>Navbar</div>
+        <div className={styles.container}>
+            <div className={styles.social}>
+                <Image src="/instagram.png" alt="instagram" width={24} height={24}/>
+                <Image src="/twitter.png" alt="twitter" width={24} height={24}/>
+                <Image src="/linkedin.png" alt="linkedin" width={24} height={24} />
+                <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+            </div>
+            <div className={styles.logo}>EOS 13 Studios</div>
+            <div className={styles.links}>
+                <ThemeToggle/>
+                <Link href="/">Home</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">About</Link>
+                <AuthLinks/>
+            </div>
+        </div>
     )
 }
 
