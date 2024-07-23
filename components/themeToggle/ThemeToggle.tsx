f3888d1ea22be7c5bf20ff70b115ae1f1e46baff
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-07-23 15:38:27 +0530

    theme

--- components/themeToggle/ThemeToggle.tsx
+++ components/themeToggle/ThemeToggle.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './themeToggle.module.css'
+import Image from 'next/image'
 
 const ThemeToggle = () => {
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="moon" width={16} height={16}/>
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="sun" width={16} height={16}/>
+    </div>
   )
 }
 
