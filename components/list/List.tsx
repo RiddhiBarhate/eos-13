57d91b9811b7362d5823200081c05771558e8f53
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-09-01 16:02:01 +0530

    list updated

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './list.module.css'
 import Information from '../information/Information'
+import Card from '../card/Card'
 
 const List = () => {
     return (
-        <div className={styles.container}>List
+        <div className={styles.container}>
+            <h1 className={styles.title}>Recent Posts</h1>
+            <div className={styles.posts}>
+                <Card/>
+                <Card />
+                <Card />
+                <Card />
+            </div>
             <Information/>
         </div>
     )
